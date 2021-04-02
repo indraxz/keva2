a8edbddc683d884eef6d78a787a2f551a7729c58
indraxz <indraskd80@gmail.com>
2021-04-02 21:31:15 +0700

    Add files via upload

---
+++ start_tes.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+reset
+
+./SRBMiner-MULTI --algorithm randomkeva --pool kevacoin.herominers.com:10140 --wallet VKz1fvznuaNFY57asDs925Jyn7m9dXWQyM --password x --cpu-threads 0 --disable-gpu 
